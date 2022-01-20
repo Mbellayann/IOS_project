05ad295887d1aad15e370a6cfd5ef5b00a4e5473
yann <mbellaonguene@gmail.com>
2022-01-20 21:01:00 +0100

    Initial Commit

---
+++ CountryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountryApp
+//
+//  Created by Ingrid bikoli on 20/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
